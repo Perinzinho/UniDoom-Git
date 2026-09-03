eccf33656c8ab315391346ac8ed85b2bdb70c1f8
Leonardo Perin Andreozi <leonardoperingo@gmail.com>
2026-09-03 20:28:08 -0300

    feat: Adicionado gravidade ao player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,32 +2,31 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
     public float speed = 6f;
-    
-    
+    public float gravity = -9.81f;
+    public float groundedGravity = -2f; // pequena força pra manter "grudado" no chão
+
     private CharacterController controller;
     private Vector3 velocity;
-    
+
     public Transform cameraTransform;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         MoveHorizontal();
+        ApplyGravity();
     }
-    
+
     void MoveHorizontal()
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        // pega a direção da câmera, mas zera o eixo Y pra não "voar"
         Vector3 camForward = cameraTransform.forward;
         camForward.y = 0f;
         camForward.Normalize();
@@ -39,5 +38,20 @@ public class PlayerMovement : MonoBehaviour
         Vector3 move = camRight * x + camForward * z;
         controller.Move(move * speed * Time.deltaTime);
     }
-}
 
+    //Faz player cair no chão
+    void ApplyGravity()
+    {
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            // reseta a velocidade vertical quando está no chão
+            velocity.y = groundedGravity;
+        }
+        else
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
