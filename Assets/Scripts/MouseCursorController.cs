950e3066f14ffbd2776e808927a4c71283757473
Leonardo Perin <155500125+Perinzinho@users.noreply.github.com>
2026-09-10 15:39:20 -0300

    feat: added sound effects

--- Assets/Scripts/MouseCursorController.cs
+++ Assets/Scripts/MouseCursorController.cs
@@ -6,7 +6,7 @@ public class MouseController : MonoBehaviour
     void Start()
     {
     Cursor.lockState = CursorLockMode.Locked;
-    Cursor.visible = false; // esconde o cursor do SO, o crosshair vira 100% UI agora
+    Cursor.visible = false; // esconde o cursor do SO, o crosshair vira UI agora
     }
 
 }
